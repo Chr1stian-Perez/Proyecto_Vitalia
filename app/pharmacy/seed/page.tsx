87d8e0da3b3577a8b5517341c922df2c9aab71b8
Dilan Andrade <DilanAndrade007>
2025-08-06 13:11:13 -0500

    Cambios en seed de pharmacy y products

--- app/pharmacy/seed/page.tsx
+++ app/pharmacy/seed/page.tsx
@@ -1,15 +1,3 @@
-// "use client"
-// import { useEffect } from "react"
-// import { seedPharmacies } from "@/lib/firebase-pharmacies"
-
-// export default function SeedPharmacyPage() {
-//   useEffect(() => {
-//     seedPharmacies()
-//   }, [])
-
-//   return (
-//     <div className="p-6 text-green-800 font-bold text-xl">
-//       Ejecutando seed de farmacias...
-//     </div>
-//   )
-// }
+export default function Page() {
+  return <div>Seed inactivo en producción.</div>
+}
